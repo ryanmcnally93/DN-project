77e9882a213d08834e02abb3fd5129f20a998393
Ryan McNally <ryanmcnallymusic@outlook.com>
2024-02-20 17:05:30 +0000

    All page links updated

--- src/assets/script.js
+++ src/assets/script.js
@@ -30,13 +30,15 @@ function GetSectionContent(sectionName){
     if(sectionName == "main")
         return MainContent();
     else if(sectionName == "employer-main")
-        return EmployerMainContent();
+        return EmployerMain();
     else if(sectionName == "employer-profile")
-        return EmployerProfileContent();
-    else if(sectionName == "section2")
-        return Section2Content();
-    else if(sectionName == "section3")
-        return Section3Content();
+        return EmployerProfile();
+    else if(sectionName == "applicant-profile")
+        return ApplicantProfile();
+    else if(sectionName == "applicant-job-view")
+        return ApplicantJobView();
+    else if(sectionName == "employer-job-view")
+        return EmployerJobView();
     else
         return SectionNotFoundContent();
 }
@@ -46,7 +48,7 @@ function MainContent(){
     return `
     <div class="section-nav">
         <a href="#employer-profile" class="nav-link green-btn">Employer Profile</a>
-        <a href="#section2" class="nav-link green-btn">Applicant Profile</a>
+        <a href="#applicant-profile" class="nav-link green-btn">Applicant Profile</a>
     </div>
     <table cellspacing=0>
         <thead>
@@ -72,7 +74,7 @@ function MainContent(){
                 <td></td>
                 <td></td>
                 <td></td>
-                <td><a href="#section3" id="details-link" class="nav-link green-btn">Details</a></td>
+                <td><a href="#applicant-job-view" id="details-link" class="nav-link green-btn">Details</a></td>
             </tr>
             <tr>
                 <td></td>
@@ -145,8 +147,11 @@ function MainContent(){
     `;
 }
 
-function EmployerMainContent(){
+function EmployerMain(){
     return `
+    <div class="left-nav">
+        <a href="#main" class="nav-link green-btn">Home</a>
+    </div>
     <div class="title"><p>Welcome EMPLOYER</p></div>
     <div class="section-nav">
         <a href="#" class="nav-link green-btn">Login</a>
@@ -176,7 +181,7 @@ function EmployerMainContent(){
                 <td></td>
                 <td></td>
                 <td></td>
-                <td><a href="#section3" id="details-link" class="nav-link green-btn">Manage</a></td>
+                <td><a href="#employer-job-view" id="details-link" class="nav-link green-btn">Manage</a></td>
             </tr>
             <tr>
                 <td></td>
@@ -231,14 +236,14 @@ function EmployerMainContent(){
                 <td></td>
                 <td></td>
                 <td></td>
-                <td><a href="#section3" id="details-link" class="nav-link green-btn">Add New</a></td>
+                <td><a href="#employer-job-view" id="details-link" class="nav-link green-btn">Add New</a></td>
             </tr>
         </tbody>
     </table>
     `;
 }
 
-function EmployerProfileContent(){
+function EmployerProfile(){
     return `
     <div class="left-nav">
         <div>
@@ -291,25 +296,148 @@ function EmployerProfileContent(){
     `;
 }
 
-function Section2Content(){
+function ApplicantProfile(){
     return `
-    <p class="section-title">Section 2</p>
-    <div class="section-nav">
-    <a href="#employer-profile" class="nav-link green-btn">Section 1</a>
-    <a href="#section2" class="nav-link green-btn btn-disabled">Section 2</a>
-    <a href="#section3" class="nav-link green-btn">Section 3</a>
+    <div class="left-nav">
+        <div>
+            <p>Welcome: NAME</p>
+        </div>
+        <a href="#main" class="nav-link green-btn">Back</a>
     </div>
+    <table cellspacing=0 id="employer-profile-table">
+        <thead>
+            <tr>
+                <th>Applicant Name:</th>
+                <th>Data</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Phone Number</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Profile Picture</td>
+                <td>Image / File Load Paste</td>
+            </tr>
+            <tr>
+                <td>CV</td>
+                <td>Image / File Load Paste</td>
+            </tr>
+        </tbody>
+    </table>
     `;
 }
 
-function Section3Content(){
+function ApplicantJobView(){
     return `
-    <p class="section-title">Section 3</p>
-    <div class="section-nav">
-    <a href="#employer-profile" class="nav-link green-btn">Section 1</a>
-    <a href="#section2" class="nav-link green-btn">Section 2</a>
-    <a href="#section3" class="nav-link green-btn btn-disabled">Section 3</a>
+    <a href="#applicant-profile" class="nav-link green-btn" id="applicant-profile-link">Applicant Profile</a>
+    <div class="left-nav">
+        <div>
+            <a href="#" class="nav-link green-btn">Apply</a>
+        </div>
+        <a href="#main" class="nav-link green-btn">Back</a>
     </div>
+    <table cellspacing=0 id="employer-profile-table">
+        <thead>
+            <tr>
+                <th>Company Name</th>
+                <th>Data</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Role Category</td>
+                <td>Drop Down</td>
+            </tr>
+            <tr>
+                <td>Role</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Location</td>
+                <td>Combi Box</td>
+            </tr>
+            <tr>
+                <td>Industry</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Function</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Title</td>
+                <td>Image / File Load Paste</td>
+            </tr>
+            <tr>
+                <td>Experience</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Salary</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    `;
+}
+
+function EmployerJobView(){
+    return `
+    <div class="left-nav">
+        <div>
+            <p>Welcome: NAME</p>
+        </div>
+        <a href="#" class="nav-link green-btn">Save</a>
+        <a href="#employer-main" class="nav-link green-btn">Back</a>
+    </div>
+    <table cellspacing=0 id="employer-profile-table">
+        <thead>
+            <tr>
+                <th>Company Name</th>
+                <th>Data</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Role Category</td>
+                <td>Drop Down</td>
+            </tr>
+            <tr>
+                <td>Role</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Location</td>
+                <td>Combi Box</td>
+            </tr>
+            <tr>
+                <td>Industry</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Function</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Title</td>
+                <td>Image / File Load Paste</td>
+            </tr>
+            <tr>
+                <td>Experience</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Salary</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
     `;
 }
 
@@ -317,8 +445,7 @@ function SectionNotFoundContent(){
     return `
     <p class="section-title">Section was not found. Please Select a Section</p>
     <div class="section-nav">
-        <a href="#employer-profile" class="nav-link green-btn">Section 1</a>
-        <a href="#section2" class="nav-link green-btn">Section 2</a>
-        <a href="#section3" class="nav-link green-btn">Section 3</a>
+        <a href="#employer-profile" class="nav-link green-btn">Employer Profile</a>
+        <a href="#applicant-profile" class="nav-link green-btn">Applicant Profile</a>
     </div> `;
 }
